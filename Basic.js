3eb857192dafa9a5587a3f6446478aac7510fcd2
Deepshikha <deepshika3010@gmail>
2020-08-31 12:44:12 +0530

    map ()

--- Basic.js
+++ Basic.js
@@ -1,33 +1,26 @@
-//  var para = document.querySelector('.p1').style.style.backgroundColor="red";
 
-// para.addEventListener('click', updateName);
+const arr = [{name:"Deepshikha",age:18}, {name:"Manu",age:20}]
+const person_names = arr.map(person => person.name)
+console.log(person_names);
 
-//  function updateName() {
-// let name = prompt('Enter a new name');
-// para.textContent = 'Player 1: ' + name;
-// }
 
- var Student = ["DEEPSHIKHA", "MONI",  
-                 "DEEP", "SHIBU"]; 
-            var g = Student.entries(); 
-            var f=Student.values();
-            var d=Student.keys();
-  
-            for (x of g) { 
-                document.getElementById("geeks") 
-                  .innerHTML += x + "<br>"; 
 
-            } 
-            
-            for (x of f) { 
-                document.getElementById("value") 
-                  .innerHTML += x + "<br>"; 
 
-            } 
 
-            
-            for (x of d) { 
-                document.getElementById("key") 
-                  .innerHTML += x + "<br>"; 
 
-            } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
